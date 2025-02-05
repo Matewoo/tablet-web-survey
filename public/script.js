134938a0f35d88b1495281e743f6de56da21f4ad
Mateo Körber Belmonte <44673490+Matewoo@users.noreply.github.com>
2025-02-05 15:59:07 +0100

    new submit logic
    There's no submit button anymore. Clicking on an emoji submits the results automatically

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.emoji').forEach(button => {
+/* document.querySelectorAll('.emoji').forEach(button => {
   button.addEventListener('click', () => {
     const category = button.getAttribute('data-category');
     if (category === 'Fleischgericht' || category === 'Vegetarisch' || category === 'Tagesgericht' || category === 'Tagessalat') {
@@ -44,4 +44,34 @@ document.getElementById('submit').addEventListener('click', () => {
   setTimeout(() => {
     submitButton.disabled = false;
   }, 1000);
+}); */
+
+document.querySelectorAll('.emoji').forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.getAttribute('data-category');
+    const results = {};
+    results[category] = button.id;
+
+    fetch('/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(results)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      document.getElementById('thank-you-message').style.display = 'block';
+      document.getElementById('content').style.visibility = 'hidden';
+      setTimeout(() => {
+        document.getElementById('thank-you-message').style.display = 'none';
+        document.getElementById('content').style.visibility = 'visible';
+        document.querySelectorAll('.emoji').forEach(button => button.classList.remove('selected'));
+      }, 1000);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  });
 });
